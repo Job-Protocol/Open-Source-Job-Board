8e2d7b140ec673695fa967c5bd58ca055530a522
Lukas Scheucher <scheuclu@gmail.com>
2023-02-01 14:19:46 +0000

    Merged changes.

---
+++ bin/sendmail.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import config from "../config.json";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // if (!process.env.BUBBLE_API_PRIVATE_KEY) {
+  //   res.status(500).json("Fail");
+  //   return;
+  // }
+
+  // var myHeaders = new Headers();
+  // myHeaders.append(
+  //   "Authorization",
+  //   "Bearer ".concat(process.env.BUBBLE_API_PRIVATE_KEY as string)
+  // );
+
+  // myHeaders.append("Content-Type", "application/json");
+
+  // var requestOptions = {
+  //   method: "POST",
+  //   headers: myHeaders,
+  //   body: req.body,
+  //   redirect: "follow",
+  // };
+
+  // const response = await fetch(
+  //   config["dev"]["endpoint"] + "/obj/candidate/",
+  //   requestOptions
+  // );
+
+  // if (response.status !== 200) {
+  //   res.status(response.status).json("Failed to create");
+  //   return;
+  // }
+
+  res.status(200).json("Mail sent");
+
+  postMessage("Mail sent");
+}
