6c89d0e40aa82c74bf23790fec5eec83b0d22f0f
Boris Gordts <BGordts@users.noreply.github.com>
2023-02-02 22:46:06 +0100

    started on design - header part is done

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ The jobboard is powered by [JobProtocol](https://www.jobprotocol.com).
 Job Protocol lets companies list open roles with a referral bounty on the blockchain, incentivizing their community and our ecosystem of talent partners - recruiters, communities, agencies, networkers - to help hire top talent faster and cheaper.
 
 ![](https://uploads-ssl.webflow.com/624328d75b9d60a4652c67d5/624328d75b9d605b1c2c68f5_Group%2520228-p-500.png)
+
+## Setup
+
+Make sure you have the following .env file:
+
+```
+BUBBLE_API_PRIVATE_KEY=
+GOOGLE_API_KEY=
+```
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from 'next/image'
 import styles from "@/styles/Home.module.css";
 import Joblist from "@/components/overview/joblist";
 import Companylist from "@/components/overview/companylist";
@@ -20,10 +21,89 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/faviconV2.png" />
       </Head>
-      <div className={styles.center}>
-        <h1 className={styles.pagetitle}>ETH Denver Job Board</h1>
-        <p>Below is the full list of open roles. Open any role for details.</p>
+      <div className={styles.page}>
+        <div className={styles.pageContainer}>
+          <div className={styles.coverImageContainer}>
+            <Image
+              src={"/background_eth_3.jpg"}
+              alt="Header image"
+              fill
+              style={{borderBottomLeftRadius: 16, borderBottomRightRadius: 16}}
+              objectFit="cover"
+            />
+          </div>
+          <div className={styles.headerContainer}>
+            <div className={styles.logoImageContainer}>
+              <Image
+                  src={"/EDEN22Logo_Black.svg"}
+                  alt="Header image"
+                  fill
+                  //style={{borderRadius: 8}}
+                  // objectFit="cover"
+                />
+            </div>
+            <div className={styles.headerTextContainer}>
+              <h1 className={styles.h1}>ETH Denver Job Board</h1>
+              <h2 className={styles.subTitle}>The best jobs in crypto</h2>
+              <div className={styles.headerIconsContainer}>
+                <Image
+                    src={"/globe.svg"}
+                    alt="Globe icon"
+                    width={16}
+                    height={16}
+                    //style={{borderRadius: 8}}
+                    // objectFit="cover"
+                  />
+                  <Image
+                    src={"/twitter.svg"}
+                    alt="Twitter icon"
+                    width={16}
+                    height={16}
+                    //style={{borderRadius: 8}}
+                    // objectFit="cover"
+                  />
+                  <Image
+                    src={"/facebook.svg"}
+                    alt="Twitter icon"
+                    width={16}
+                    height={16}
+                    //style={{borderRadius: 8}}
+                    // objectFit="cover"
+                  />
+                  <Image
+                    src={"/linkedin-square-colored.svg"}
+                    alt="Twitter icon"
+                    width={16}
+                    height={16}
+                    //style={{borderRadius: 8}}
+                    // objectFit="cover"
+                  />
+                  <Image
+                    src={"/Discord_Logo_sans_texte 1.svg"}
+                    alt="Twitter icon"
+                    width={16}
+                    height={16}
+                    //style={{borderRadius: 8}}
+                    // objectFit="cover"
+                  />
+              </div>
+            </div>
+          </div>
+          
+          <div className={styles.filtersContainer}>
+            <div className={styles.toggleButton}>
+              <div className={styles.toggleOn}>
+                Roles
+              </div>
+              <div className={styles.toggleOff}>
+                Companies
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
+      
 
       <main className={styles.main}>
         <label>
