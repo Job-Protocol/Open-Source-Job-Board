6b7a2eabd300f9729f4bb2125f4935496e38b9dc
Lukas Scheucher <scheuclu@gmail.com>
2023-02-08 15:38:45 +0000

    Added conditional rendering.

--- components/role/detail/companyinfo.tsx
+++ components/role/detail/companyinfo.tsx
@@ -30,63 +30,69 @@ export default function CompanyCard(data: CompanyCardProps) {
         <p className={"body16"}>{data.company.tagline}</p>
       </div>
 
-      <div className={styles.section}>
-        <h4 className={"chapeauDark"}>About</h4>
-        <div className={styles.aboutItems}>
-          <div className={"body16 " + styles.aboutItem}>
-            <FaUserFriends />
-            <p>{data.company.num_employees} employees</p>
-          </div>
+      {(data.company.num_employees || data.company.headquarters) &&
+        <div className={styles.section}>
+          <h4 className={"chapeauDark"}>About</h4>
+          <div className={styles.aboutItems}>
+            <div className={"body16 " + styles.aboutItem}>
+              <FaUserFriends />
+              <p>{data.company.num_employees} employees</p>
+            </div>
 
-          <div className={"body16 " + styles.aboutItem}>
-            <FaHome />
-            <p>{data.company.headquarters}</p>
+            <div className={"body16 " + styles.aboutItem}>
+              <FaHome />
+              <p>{data.company.headquarters}</p>
+            </div>
           </div>
         </div>
-      </div>
+      }
 
-      <div className={styles.section}>
-        <h4 className={"chapeauDark"}>Socials</h4>
-        <div className={styles.socials}>
-          {data.company.socials && valid(data.company.socials.twitter) && (
-            <a href={data.company.socials.twitter}>
-              <FaTwitter />
-            </a>
-          )}
-          {data.company.socials && valid(data.company.socials.github) && (
-            <a href={data.company.socials.github}>
-              <FaGithub />
-            </a>
-          )}
-          {data.company.socials && valid(data.company.socials.linkedin) && (
-            <a href={data.company.socials.linkedin}>
-              <FaLinkedin />
-            </a>
-          )}
-        </div>
-      </div>
-
-      <div className={styles.section}>
-        <h4 className={"chapeauDark"}>Press</h4>
-        <div className={styles.pressLinksContainer}>
-          {data.company.press_article_links &&
-            data.company.press_article_links.map((link) => (
-              <a
-                className={"body16 " + styles.pressLink}
-                href={link.link}
-                key={link.display_name}
-              >
-                {link.display_name}
-                <Image
-                  src={"/external_link.svg"}
-                  width={16}
-                  height={16}
-                  alt="external link"
-                />
+      {data.company.socials &&
+        <div className={styles.section}>
+          <h4 className={"chapeauDark"}>Socials</h4>
+          <div className={styles.socials}>
+            {data.company.socials && valid(data.company.socials.twitter) && (
+              <a href={data.company.socials.twitter}>
+                <FaTwitter />
               </a>
-            ))}
+            )}
+            {data.company.socials && valid(data.company.socials.github) && (
+              <a href={data.company.socials.github}>
+                <FaGithub />
+              </a>
+            )}
+            {data.company.socials && valid(data.company.socials.linkedin) && (
+              <a href={data.company.socials.linkedin}>
+                <FaLinkedin />
+              </a>
+            )}
+          </div>
         </div>
-      </div>
+      }
+
+      {data.company.press_article_links &&
+        <div className={styles.section}>
+          <h4 className={"chapeauDark"}>Press</h4>
+          <div className={styles.pressLinksContainer}>
+            {data.company.press_article_links &&
+              data.company.press_article_links.map((link) => (
+                <a
+                  className={"body16 " + styles.pressLink}
+                  href={link.link}
+                  key={link.display_name}
+                >
+                  {link.display_name}
+                  <Image
+                    src={"/external_link.svg"}
+                    width={16}
+                    height={16}
+                    alt="external link"
+                  />
+                </a>
+              ))}
+          </div>
+        </div>
+      }
     </div>
   );
 }
