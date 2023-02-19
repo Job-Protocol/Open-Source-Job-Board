04be344e36104765b5787f0aa4c7a2fce395435f
Lukas Scheucher <scheuclu@gmail.com>
2023-02-19 18:46:34 +0000

    Implemented 404 page.

--- pages/404.tsx
+++ pages/404.tsx
@@ -23,7 +23,6 @@ export default function FourOhFour() {
                             Click here to go back home ...
                         </Link>
 
-
                     </div>
                 </div>
             </div>
