71a5a28a442d3e65dc8ad219d0a812a3c46f7182
Lukas Scheucher <scheuclu@gmail.com>
2023-02-24 22:02:37 +0100

    DEBUG

---
+++ parse_yaml.py
@@ -0,0 +1,50 @@
+
+with open('file.yaml') as f:
+    lines = f.readlines()
+    
+
+
+d = {}
+
+def count_leading_spaces(line):
+    
+    for lead in range(100,0,-1):
+        if line.startswith(''.join([' ']*lead)):
+            return lead
+    return 0
+
+
+prefixes=[]
+oldindent=0
+key=""
+
+out=[]
+
+indent2index={0: 0}
+for line in lines:
+    indent = count_leading_spaces(line)
+    if indent>oldindent:
+        prefixes.append(key)
+        indent2index[indent]=len(prefixes)
+        
+    if indent<oldindent:
+        prefixes = prefixes[:indent2index[indent]]
+        
+    
+    oldindent = indent
+    
+    key, val = line.strip().split(':')
+    
+    if val:
+        outline=f"export {'NEXT_PUBLIC_'+'_'.join(prefixes+[key])}='{val.strip()}'"
+        out.append(outline+'\n')
+        print(outline)
+
+
+with open('temp.sh', 'w') as f:
+    f.writelines(out)
+        
+    
+    
+    
+    
---
+++ run_x.sh
@@ -0,0 +1,9 @@
+for p in $(python3 parse_yaml.py)
+do
+    echo XX_$p
+done
+
+
+eval($(python3 parse_yaml.py)
+
+printenv
