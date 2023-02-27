3b80db42f64d3cafcb849bb6821095564c5c5507
Lukas Scheucher <scheuclu@gmail.com>
2023-02-27 10:48:47 +0100

    Implemented List navigator.

--- components/overview/joblist.tsx
+++ components/overview/joblist.tsx
@@ -21,12 +21,24 @@ import {
   RoleLocationType,
   TimezoneRange,
 } from "@/bubble_types";
+import PageSelector from "./pageselector";
 
 export interface Props {
   roles: Role[] | undefined;
+  numItemsPerPage: number;
 }
 
 export default function Joblist(data: Props) {
+
+
+  const [firstItemIndex, setFirstItemIndex] = useState<number>(0);
+  const [lastItemIndex, setLastItemIndex] = useState<number>(data.roles ? data.roles.length - 1 : 0);
+
+  useEffect(() => {
+    console.log("Data changed. Reset first item index to 0");
+    setFirstItemIndex(0);
+    setLastItemIndex((data.roles ? data.roles.length - 1 : 0));
+  }, [data]);
 
 
   if (!data.roles) return (<Loading />);
@@ -36,9 +48,22 @@ export default function Joblist(data: Props) {
 
   return (
     <div className={styles.jobListContainer}>
-      {roles.map((role) => (
-        <JobCard role={role} key={role.id} />
+      {/* <p>Lenght of role in joblist {data.roles.length} {firstItemIndex} {lastItemIndex}</p> */}
+
+      {roles.slice(firstItemIndex, lastItemIndex + 1).map((role, index) => (
+        <div key={index}>
+          {/* <p>{index}</p> */}
+          <JobCard role={role} key={role.id} />
+        </div>
       ))}
+      <PageSelector
+        numItems={data.roles.length}
+        numItemsPerPage={data.numItemsPerPage}
+        handlePageChange={(firstItemIndex: number, lastItemIndex: number) => {
+          setFirstItemIndex(firstItemIndex);
+          setLastItemIndex(lastItemIndex);
+        }}
+      />
     </div>
   );
 }
