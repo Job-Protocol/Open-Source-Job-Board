ca9efa5b4552f936d1dfdbda57a9d874ecf66bdc
Lukas Scheucher <scheuclu@gmail.com>
2023-03-22 08:42:00 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -106,7 +106,7 @@ export async function getStaticProps() {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 10 seconds
-    revalidate: 60 * 30, // In seconds
+    revalidate: 60 * 60 * 24, // In seconds
   };
 }
 
